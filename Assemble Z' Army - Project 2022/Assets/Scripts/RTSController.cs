509b4f89fc41294fd393d8ad982bbe1bc54d93e3
Eliachar Feig <efeig15@gmail.com>
2021-12-03 19:33:40 +0200

    some animation on a unit when moving

--- Assemble Z' Army - Project 2022/Assets/Scripts/RTSController.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/RTSController.cs
@@ -7,32 +7,53 @@ public class RTSController : MonoBehaviour
 {
     private void Awake()
     {
-       selectedUnits = new List<Unit>(); 
+        selectedUnits = new List<Unit>();
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(0))
+        {
             selectedUnits.Clear();
             startPos = Utils.GetMouseWorldPosition();
         }
 
-        if(Input.GetMouseButtonUp(0)){
+        if (Input.GetMouseButtonUp(0))
+        {
             Collider2D[] inChosenArea = Physics2D.OverlapAreaAll(startPos, Utils.GetMouseWorldPosition());
-            foreach(Collider2D obj in inChosenArea){
+            foreach (Collider2D obj in inChosenArea)
+            {
                 Unit unit = obj.GetComponent<Unit>();
-                if(unit != null && unit.isSelectable()){
+                if (unit != null && unit.isSelectable())
+                {
                     selectedUnits.Add(unit);
                     Debug.Log(unit.transform.position);
                 }
             }
             Debug.Log("RTS says hi");
         }
-        
-        if(Input.GetMouseButtonDown(1)){
-                foreach(Unit unit in selectedUnits)
-                    unit.MoveTo(Utils.GetMouseWorldPosition());
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            foreach (Unit unit in selectedUnits)
+            {
+               // unit.setDest(Utils.GetMouseWorldPosition());
+                unit.MoveTo(Utils.GetMouseWorldPosition());
+            }
         }
+
+        foreach (Unit unit in selectedUnits)
+        {
+            Debug.Log("unit pos: " + unit.transform.position);
+            Debug.Log("dest========== " + unit.getDest());
+            if (unit.transform.position.x == unit.getDest().x 
+            && unit.transform.position.y == unit.getDest().y){
+                Debug.Log("true");
+                unit.Stop();
+            }
+                
+        } 
+
     }
     private List<Unit> selectedUnits;
     private Vector3 startPos;
--- Assemble Z' Army - Project 2022/Assets/Scripts/Unit.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Unit.cs
@@ -9,6 +9,7 @@ public class Unit : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    private Animator myAnimator;
 
     private void Start()
     {
@@ -16,20 +17,38 @@ public class Unit : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        myAnimator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+      // FlipSideSprite();
     }
 
     public void MoveTo(Vector3 dest)
     {
+        myAnimator.SetBool("isRunning", true);
+
         Debug.Log("dest:" + dest);
         dest.z = 0;
         agent.SetDestination(dest);
+        
+    }
+
+    public Vector3 getDest(){
+        return agent.destination;
+    }
+
+    public void Stop(){
+        myAnimator.SetBool("isRunning", false);
     }
 
     public bool isSelectable(){
         return selectable;
     }
+
+    // public void FlipSideSprite(){
+    //     transform.localScale = new Vector3 (Mathf.Sign(agent.velocity.x), 1f);
+    // }
 }
