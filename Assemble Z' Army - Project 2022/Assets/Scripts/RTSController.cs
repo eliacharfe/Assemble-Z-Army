94207db3482a8bbca1128aaa612b58475472316c
Eliachar Feig <efeig15@gmail.com>
2021-12-09 19:09:28 +0200

    feedback on selection unit

--- Assemble Z' Army - Project 2022/Assets/Scripts/RTSController.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/RTSController.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Utilities;
+using UnityEngine.InputSystem;
+using System;
 
 public class RTSController : MonoBehaviour
 {
+    private List<Unit> selectedUnits;
+    private Vector3 startPos;
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+    private Camera mainCamera;
+
     private void Awake()
     {
+        mainCamera = Camera.main;
         selectedUnits = new List<Unit>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            selectedUnits.Clear();
-            startPos = Utils.GetMouseWorldPosition();
-        }
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     selectedUnits.Clear();
+        //     startPos = Utils.GetMouseWorldPosition();
+        // }
 
         if (Input.GetMouseButtonUp(0))
         {
@@ -27,39 +35,59 @@ public class RTSController : MonoBehaviour
                 if (unit != null && unit.isSelectable())
                 {
                     selectedUnits.Add(unit);
-                    unit.SetColorSelcted();
-                  // Debug.Log(unit.transform.position);
+                    ///  unit.SetColorSelcted();
+                    unit.Select();
                 }
-                // else {
-                //     foreach (Unit un in selectedUnits)
-                //        un.ResetColor();
-                // }
             }
-           // Debug.Log("RTS says hi");
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            foreach (Unit unit in selectedUnits){
+            foreach (Unit unit in selectedUnits)
+            {
                 unit.MoveTo(Utils.GetMouseWorldPosition());
                 unit.ResetColor();
             }
-        
+
         }
 
         foreach (Unit unit in selectedUnits)
         {
-           // Debug.Log("unit pos: " + unit.transform.position);
-          //  Debug.Log("dest========== " + unit.getDest());
-            if (unit.transform.position.x == unit.getDest().x 
-            && unit.transform.position.y == unit.getDest().y){
-              //  Debug.Log("true");
+            // Debug.Log("unit pos: " + unit.transform.position);
+            //  Debug.Log("dest========== " + unit.getDest());
+            if (unit.transform.position.x == unit.getDest().x
+            && unit.transform.position.y == unit.getDest().y)
+            {
+                //  Debug.Log("true");
                 unit.Stop();
             }
-                
-        } 
 
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            StartSelectionArea();
+
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            // ClearSelectionArea();
+        }
     }
-    private List<Unit> selectedUnits;
-    private Vector3 startPos;
+//-------------------------------------
+    private void StartSelectionArea()
+    {
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Deselect();
+        }
+
+        selectedUnits.Clear();
+        startPos = Utils.GetMouseWorldPosition();
+    }
+
+    // private void ClearSelectionArea()
+    // {
+    //     throw new NotImplementedException();
+    // }
 }
--- Assemble Z' Army - Project 2022/Assets/Scripts/Unit.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Unit.cs
@@ -1,21 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Unit : MonoBehaviour
 {
     private bool selectable;
 
     private NavMeshAgent agent;
-
     private Animator myAnimator;
     private SpriteRenderer spriteRenderer;
 
-    // private bool unitSelected;
-    // public static bool dragSelectedUnitAllowed, mouseOverUnit;
-    // private Vector2 mousePos;
-    // private float dragOffsetX, dragOffsetY;
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
 
     private void Start()
     {
@@ -27,91 +27,59 @@ public class Unit : MonoBehaviour
         myAnimator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        // unitSelected = false;
-        // dragSelectedUnitAllowed = false;
-        // mouseOverUnit = false;
+
     }
 
     private void Update()
     {
-        // if (Input.GetMouseButtonDown(0)){
-        //     dragOffsetX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
-        //     dragOffsetY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
-        // }
 
-        // if(Input.GetMouseButton(0))
-        //     mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // if (unitSelected && dragSelectedUnitAllowed)
-        //     transform.position = new Vector2(mousePos.x - dragOffsetX, mousePos.y - dragOffsetY);
-
-        // if (Input.GetMouseButtonDown(1)){
-        //     unitSelected = false;
-        //     dragSelectedUnitAllowed = false;
-        //     spriteRenderer.color = new Color(1f,1f,1f,1f);
-        // }
-      // FlipSideSprite();
     }
 
     public void MoveTo(Vector3 dest)
     {
         myAnimator.SetBool("isRunning", true);
 
-       // Debug.Log("dest:" + dest);
+        // Debug.Log("dest:" + dest);
         dest.z = 0;
         agent.SetDestination(dest);
-        
+
     }
 
-    public Vector3 getDest(){
+    public Vector3 getDest()
+    {
         return agent.destination;
     }
 
-    public void Stop(){
+    public void Stop()
+    {
         myAnimator.SetBool("isRunning", false);
     }
 
-    public void SetColorSelcted(){
-        spriteRenderer.color = new Color(1f,0f,0f,1f);
+    public void SetColorSelcted()
+    {
+        spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
     }
 
-    public void ResetColor(){
-        spriteRenderer.color = new Color(1f,1f,1f,1f);
+    public void ResetColor()
+    {
+        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
     }
 
-    public bool isSelectable(){
+    public bool isSelectable()
+    {
         return selectable;
     }
 
-    // private void OnTriggerEnter2D(Collider2D collision){
-    //     if (collision.gameObject.GetComponent<BoxCollection>()){
-    //         spriteRenderer.color = new Color(1f,0f,0f,1f);
-    //         unitSelected = true;
-    //     }
-    // }
+    public void Select()
+    {
+        onSelected?.Invoke();
+    }
 
-    // private void OnMouseDown(){
-    //     mouseOverUnit = true;
-    // }
-
-    //  private void OnMouseUp(){
-    //     mouseOverUnit = false;
-    //     dragSelectedUnitAllowed = false;
-    // }
-
-    // private void OnMouseDrag(){
-    //     dragSelectedUnitAllowed = true;
-
-    //     if (!unitSelected){
-    //         dragSelectedUnitAllowed = false;
-    //     }
-      
-    //   mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //   transform.position = new Vector2(mousePos.x - dragOffsetX, mousePos.y - dragOffsetY);
-    // }
+    public void Deselect()
+    {
+        onDeselected?.Invoke();
+    }
 
 
-    // public void FlipSideSprite(){
-    //     transform.localScale = new Vector3 (Mathf.Sign(agent.velocity.x), 1f);
-    // }
+
 }
