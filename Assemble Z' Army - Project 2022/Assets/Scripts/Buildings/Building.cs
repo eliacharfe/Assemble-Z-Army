b9d7045cdb56e675796198d87253da933e7e837e
Shimshon <shimpolak@gmail.com>
2021-12-19 14:40:33 +0200

    Created unit waiting list for building, Added UI on building selection
    Todo-
    Set construction preview building.
    Allow worker build  the required  building.
    Check about commending unit to move by code.

--- Assemble Z' Army - Project 2022/Assets/Scripts/Buildings/Building.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Buildings/Building.cs
@@ -1,24 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
-using System;
-using UnityEngine.AI;
-using UnityEngine.UI;
-using UnityEditor;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Building : MonoBehaviour
 {
-    [SerializeField] Unit unitInPrefab = null;
-    [SerializeField] Unit unitOutPrefab = null;
+    // Type of solider building recived and create.
+    [SerializeField] Unit unitInPrefab = null;// TODO - set array of possibles options.
+    [SerializeField] Unit unitOutPrefab = null;// Create a class which create the wanted unit according.
 
     [SerializeField] float spawnTime = 5f;
 
     [SerializeField] Transform spawnPoint = null;
+    
     [SerializeField] GameObject token = null;
+    [SerializeField] UnitSlider timeSlider = null;
 
-    [SerializeField]UnitSlider timeSlider = null;
+    private List<Unit> waitingUnit = new List<Unit>();
 
     private bool inProgess = false;
     private float timeLeft = 0;
@@ -35,36 +34,82 @@ public class Building : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(inProgess)
+        if(Input.GetMouseButtonDown(0))
         {
-            if (timeLeft / spawnTime < 1)
-            {
-                timeSlider.setFill(timeLeft / spawnTime);
-                timeLeft += Time.deltaTime;
-            }
-            else
-            {
-                timeLeft = 0f;
-                spawnNewUnit();
-                inProgess = false;
-                timeSlider.resetSlider();
-            }
+            token.SetActive(false);
+
+            showBuildingPanel(false);
         }
 
-   
+        if (!inProgess)
+        {
+            return;
+        }
 
+        if (timeLeft / spawnTime < 1)
+        {
+            timeSlider.setFill(timeLeft / spawnTime);
+            timeLeft += Time.deltaTime;
+        }
+        else
+        {
+            spawnNewUnit();
+
+            FreeBuildingSpace();
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        token.SetActive(true);
+
+        showBuildingPanel(true);
 
     }
+
+    private void OnMouseEnter()
+    {
+        if (!token.activeSelf)
+        {
+            gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.grey;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        tryToRecruitUnit(collision);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Unit unit = collision.gameObject.GetComponent<Unit>();
+
+        if (unit.waitingToBeRecruited && unit.tag == unitInPrefab.tag)
+        {
+            if(!waitingUnit.Contains(unit))
+            {
+                waitingUnit.Add(unit);
+            }
+
+        }
+    }
+
 
     public Sprite GetBuildingSprite()
     {
         return GetComponent<Sprite>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Try to recruit if building is not middle of recruitment.
+    void tryToRecruitUnit(Collider2D collision)
     {
         Unit unit = collision.gameObject.GetComponent<Unit>();
-        Debug.Log(inProgess);
+
         if (!unit || inProgess)
         {
             return;
@@ -74,18 +119,50 @@ public class Building : MonoBehaviour
         if (unit.waitingToBeRecruited && unit.tag == unitInPrefab.tag)
         {
             inProgess = true;
+
+            waitingUnit.Remove(unit);
+
             // Todo - destroy unit and add it to player list.
             Destroy(unit.gameObject);
+
+            
         }
         else
         {
-            Debug.Log("Unit can not be recruited here(Check the tag fit the building unit)." + unit.name);
+            Debug.Log("Unit can not be recruited here(Check the tag fit the building unit)." 
+                + unit.name);
         }
     }
 
+
+    //Spawn new unit.
     private void spawnNewUnit()
     {
         Unit unit = Instantiate(unitOutPrefab, spawnPoint.position, Quaternion.identity) as Unit;
+
+        // To do - move the unit away from the building.
+        //unit.MoveTo(new Vector3(spawnPoint.position.x+100f, 0,0));
+        
     }
 
+
+    // Intilize recruitment data time and allow recruit other unit.
+    private void FreeBuildingSpace()
+    {
+        timeLeft = 0f;
+        inProgess = false;
+        timeSlider.resetSlider();
+    }
+
+    // Show the building panel info for description.
+    private void showBuildingPanel(bool value)
+    {
+        BuildinPanelDisplay panel = FindObjectOfType<BuildinPanelDisplay>();
+
+        if (panel)
+        {
+            panel.GetComponent<Image>().enabled = value;
+        }
+
+    }
 }
