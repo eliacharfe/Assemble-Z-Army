a4647d489061f4feb275ed163185607ba2a2b868
Eliachar Feig <efeig15@gmail.com>
2022-04-28 17:24:49 +0300

    Create Help panel
    In MenuScene when player click on help button a window with scrollable text with the instruction.
    
    (Need to write the real instructions inside).

---
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Menus/HelpWindow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpWindow : MonoBehaviour
+{
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private GameObject helpPanel = null;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ActivateHelpPanel()
+    {
+      //  landingPagePanel.SetActive(false);
+        helpPanel.SetActive(true);
+
+    }
+
+     public void DeactivateHelpPanel()
+    {
+        //landingPagePanel.SetActive(true);
+        helpPanel.SetActive(false);
+
+    }
+}
