880f8edf2b852a4c6acb2d9763289ed84ca50954
Shimshon polak <shimpolak@gmail.com>
2022-05-14 14:07:32 +0300

    Added spear bonus damage, fix detection bug

--- Assemble Z' Army - Project 2022/Assets/Scripts/Combat/SwordAttack.cs
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Combat/SwordAttack.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class SwordAttack : Attacker
 {
+    float bonusDamage = 0;
+
+    private void Start()
+    {
+        var spear = GetComponent<Spear>();
+        if (spear)
+            bonusDamage = spear.bonuseAttack;
+    }
+
     public override void StopAttackAnime()
     {
         GetComponent<Animator>().SetBool("isAttacking", false);
@@ -11,6 +20,11 @@ public class SwordAttack : Attacker
     {
         GetComponent<Animator>().SetBool("isRunning", false);
         GetComponent<Animator>().SetBool("isAttacking", true);
-        target.GetComponent<Health>().DealDamage((int)damage);
+
+        var preciseDamage = damage;
+        if (target.GetComponent<Horse>())
+            preciseDamage += bonusDamage;
+
+        target.GetComponent<Health>().DealDamage((int)preciseDamage);
     }
 }
---
+++ Assemble Z' Army - Project 2022/Assets/Scripts/Units/Spear.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    public float bonuseAttack = 15f;
+}
